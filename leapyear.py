229891469948aea380454206a239ed22cc0600ec
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-11-01 02:41:04 +0000

    Added code

---
+++ leapyear.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sep 2019
+# This program uses a nested if statement
+
+
+def main():
+    # this function uses a nested if statement
+
+    # input
+    year_int = int(input("Enter the year: "))
+    print("")
+
+    # process & output
+    if year_int%4:
+        if year_int%100:
+            print("That is a common year.")
+        else:
+            print("That is a common year.")
+    else:
+        print("That is a leap year.")
+
+if __name__ == "__main__":
+    main()
